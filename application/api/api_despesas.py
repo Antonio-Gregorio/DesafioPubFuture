70b0ed6b04dabe157296a9d4a2e1b377bb9df7a7
Antonio-Gregorio <antonioac.gregorio@gmail.com>
2022-01-10 22:56:32 -0300

    add: acesso api_despesas

--- application/api/api_despesas.py
+++ application/api/api_despesas.py
@@ -1,28 +1,70 @@
 import requests
-import json
+import datetime
 
 from tabulate import tabulate
 
-def getDespesas():
-    req = requests.get('http://localhost:8000/despesa/listar')
-    list = []
+def getDespesas(filtro = False,type = False, args = False, args2 = False):
 
-    list.append(['ID','Valor','Data de Pagamento','Data de Pagamento Esperado','Tipo de Despesa','Conta'])
+    if filtro == False:
 
-    for x in req.json():
-        if x != []:
-            list.append([x['id'],x['valor'],x['dataPagamento'],x['dataPagamentoEsperado'],x['tipoPagamento'],x['conta']])
+        req = requests.get('http://localhost:8000/despesa/listar')
+        list = []
 
-    print(tabulate(list))
+        list.append(['ID','Valor','Data de Pagamento','Data de Pagamento Esperado','Tipo de Despesa','Conta'])
+
+        for x in req.json():
+                if x != []:
+                        list.append([x['id'],x['valor'],x['dataPagamento'],x['dataPagamentoEsperado'],x['tipoPagamento'],x['conta']])
+
+        print(tabulate(list))
     
+    else:
+            if type == '1':
+                dataI = args.split("-")
+                dataF = args2.split("-")
 
-def postDespesas(data):
+                dataIM = datetime.datetime(int(dataI[0]), int(dataI[1]), int(dataI[2]))
+                dataFM = datetime.datetime(int(dataF[0]), int(dataF[1]), int(dataF[2]))
+
+                req = requests.get('http://localhost:8000/despesa/listar')
+                list = []
+
+                list.append(['ID','Valor','Data de Pagamento','Data de Pagamento Esperado','Tipo de Despesa','Conta'])
+
+                for x in req.json():
+                        if x != []:
+                                DataA = str(x['dataRecebimento']).split('-')
+                                DataAtual = datetime.datetime(int(DataA[0]), int(DataA[1]), int(DataA[2]))
+
+                                if  DataAtual >= dataIM and DataAtual <= dataFM:
+                                        list.append([x['id'],x['valor'],x['dataPagamento'],x['dataPagamentoEsperado'],x['tipoPagamento'],x['conta']])
+
+                print(tabulate(list))
+
+            elif type == '2':
+                req = requests.get('http://localhost:8000/despesa/listar')
+                list = []
+
+                list.append(['ID','Valor','Data de Pagamento','Data de Pagamento Esperado','Tipo de Despesa','Conta'])
+
+                for x in req.json():
+                        if x != []:
+                                if x['tipoDespesa'] == args:
+                                        list.append([x['id'],x['valor'],x['dataPagamento'],x['dataPagamentoEsperado'],x['tipoPagamento'],x['conta']])
+
+                print(tabulate(list))
+
+
+def postDespesa(data):
         req = requests.post('http://localhost:8000/despesa/cadastrar', json=data)
 
-def putDespesas(data):
+def putDespesa(data):
         req = requests.put('http://localhost:8000/despesa/editar', json=data)
 
-def delDespesas(data):
+def delDespesa(data):
         req = requests.delete('http://localhost:8000/despesa/deletar', json=data)
+
+
+
 
 
--- application/api/api_receitas.py
+++ application/api/api_receitas.py
@@ -1,6 +1,5 @@
 import requests
 import datetime
-import json
 
 from tabulate import tabulate
 
